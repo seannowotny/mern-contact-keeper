6f4936015c86bdd0e5d381e243ad7f79ec6fe969
seannowotny <seannowotny@live.de>
2019-10-05 16:50:26 +0200

    Using flow in react front end from now on

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,7 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
 
-const Navbar = ({ title, icon }) => {
+// @flow
+
+type NavbarProps = {|
+   title: string,
+   icon: string   
+|}
+
+const Navbar = ({ title, icon }: NavbarProps) => {
    return (
       <div className="navbar bg-primary">
          <h1>
